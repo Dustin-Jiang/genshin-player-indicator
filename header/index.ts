804a1fba2ebb563f7c9ee38db218bf8fd92f5ca2
Dustin Jiang <dustinjiang@outlook.com>
2022-09-14 18:28:48 +0800

    ci: add GitHub workflow to check at each commit

--- header/index.ts
+++ header/index.ts
@@ -1,33 +1,22 @@
-import {GmFunctions, RunAt, UserScript} from "./UserScript";
+import { GmFunctions, RunAt, UserScript } from "./UserScript";
 import build from "./build";
+import gitSemverTags = require("git-semver-tags");
 
-// ==UserScript==
-// @name         原神玩家指示器
-// @namespace    www.cber.ltd
-// @version      0.6.1
-// @description  B站评论区自动标注原神玩家，依据是动态里是否有原神相关内容
-// @author       xulaupuz
-// @match        https://www.bilibili.com/video/*
-// @icon         https://static.hdslb.com/images/favicon.ico
-// @connect      bilibili.com
-// @grant        GM_xmlhttpRequest
-// @license MIT
-// @run-at document-end
-// ==/UserScript==
+gitSemverTags((err, tags) => {
+  // Make `v0.6.0` to `0.6.0`
+  let latestVersion = tags[0].substring(1);
+  const script: UserScript = {
+    name: "原神玩家指示器 - 改",
+    namespace: "https://github.com/Dustin-Jiang/genshin-player-indicator",
+    description: "B站评论区自动标注原神玩家，依据是动态里是否有原神相关内容",
+    author: "xulaupuz, Dustin Jiang",
+    version: latestVersion,
+    matches: ["https://www.bilibili.com/video/*"],
+    icon: "https://static.hdslb.com/images/favicon.ico",
+    grants: [GmFunctions.xmlhttpRequest],
+    license: "MIT",
+    runAt: RunAt.document_end,
+  };
 
-const script: UserScript = {
-  name: "原神玩家指示器 - 改",
-  namespace: "https://github.com/Dustin-Jiang/genshin-player-indicator",
-  description: "B站评论区自动标注原神玩家，依据是动态里是否有原神相关内容",
-  author: "xulaupuz, Dustin Jiang",
-  version: "0.6.1",
-  matches: [
-    "https://www.bilibili.com/video/*"
-  ],
-  icon: "https://static.hdslb.com/images/favicon.ico",
-  grants: [GmFunctions.xmlhttpRequest],
-  license: "MIT",
-  runAt: RunAt.document_end,
-};
-
-build(script,'app_header.js')
+  build(script, "app_header.js");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,41 +4,27 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 const {CheckerPlugin} = require('awesome-typescript-loader')
 const TerserPlugin = require('terser-webpack-plugin')
 const fs = require('fs')
-const argv = require('yargs').argv;
-const sync = require('./sync')
 
 // 通过npm run build:header编译出的路径
 const header_path = 'dist/app_header.js'
 
 let output = {
-    filename: 'main.js',
+    filename: 'genshin-player-indicator.userscript.js',
     path: path.resolve(__dirname, 'dist')
 }
 
 
 module.exports = async () => {
-
     if (!fs.existsSync(header_path)) {
         throw '文件' + header_path + '不存在，请先执行npm run build:header编译！'
     }
     const header = fs.readFileSync(header_path, 'utf-8')
-
-
-    if (argv.sync === 'yes') {
-        //WebDav同步模式，更改输出目标
-        output = await sync(header)
-    }
 
     return {
         mode: 'none',
 
         entry: './src/index.ts',
         output: output,
-        devServer: {
-            // 以dist为基础启动一个服务器，服务器运行在4200端口上，每次启动时自动打开浏览器
-            contentBase: 'dist',
-            port: 4200
-        },
         module: {
             rules: [{
                 test: /\.ts$/,
